c6ef274f70bf3495d223f311433204d864141d79
Pabloo17 <pabgar12@ucm.es>
2019-04-29 14:43:30 +0200

    añadidos ficheros para la creacion de tablas de palabras faciles en la base de datos

---
+++ PruebaPrototipo/prototipo/base de datos/drop_tables.sql
@@ -0,0 +1,32 @@
+
+USE `mcr30`;
+
+DROP TABLE IF EXISTS `wei_cat-30_examples`;
+DROP TABLE IF EXISTS `wei_cat-30_relation`;
+DROP TABLE IF EXISTS `wei_cat-30_synset`;
+DROP TABLE IF EXISTS `wei_cat-30_to_ili`;
+DROP TABLE IF EXISTS `wei_cat-30_variant`;
+
+DROP TABLE IF EXISTS `wei_eng-30_examples`;
+DROP TABLE IF EXISTS `wei_eng-30_relation`;
+DROP TABLE IF EXISTS `wei_eng-30_synset`;
+DROP TABLE IF EXISTS `wei_eng-30_to_ili`;
+DROP TABLE IF EXISTS `wei_eng-30_variant`;
+
+DROP TABLE IF EXISTS `wei_eus-30_examples`;
+DROP TABLE IF EXISTS `wei_eus-30_relation`;
+DROP TABLE IF EXISTS `wei_eus-30_synset`;
+DROP TABLE IF EXISTS `wei_eus-30_to_ili`;
+DROP TABLE IF EXISTS `wei_eus-30_variant`;
+
+DROP TABLE IF EXISTS `wei_glg-30_examples`;
+DROP TABLE IF EXISTS `wei_glg-30_relation`;
+DROP TABLE IF EXISTS `wei_glg-30_synset`;
+DROP TABLE IF EXISTS `wei_glg-30_to_ili`;
+DROP TABLE IF EXISTS `wei_glg-30_variant`;
+
+DROP TABLE IF EXISTS `wei_por-30_examples`;
+DROP TABLE IF EXISTS `wei_por-30_relation`;
+DROP TABLE IF EXISTS `wei_por-30_synset`;
+DROP TABLE IF EXISTS `wei_por-30_to_ili`;
+DROP TABLE IF EXISTS `wei_por-30_variant`;
