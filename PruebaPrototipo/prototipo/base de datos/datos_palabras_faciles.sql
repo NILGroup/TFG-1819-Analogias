400f63bd22ac26afd1fa6f7a3ba6baa4bed9fd78
Irene Martin <imart02@ucm.es>
2019-05-12 18:19:03 +0200

    Revert "Revert "Merge branch 'PrototipoWordNet3.0' of https://github.com/NILGroup/TFG-1819-Analogias into PrototipoWordNet3.0""
    This reverts commit 5d552138714208f93544284105a75cc65e7e5cc8.

--- PruebaPrototipo/prototipo/base de datos/datos_palabras_faciles.sql
+++
@@ -1,6 +0,0 @@
-
-USE `mcr30`;
-
-LOAD DATA LOCAL INFILE 'C:/Wordnet-BBDD/spaWN/1000_palabras_faciles.csv' INTO TABLE `1000_palabras_faciles` LINES TERMINATED BY '\r\n';
-LOAD DATA LOCAL INFILE 'C:/Wordnet-BBDD/spaWN/5000_palabras_faciles.csv' INTO TABLE `5000_palabras_faciles` LINES TERMINATED BY '\r\n';
-LOAD DATA LOCAL INFILE 'C:/Wordnet-BBDD/spaWN/10000_palabras_faciles.csv' INTO TABLE `10000_palabras_faciles` LINES TERMINATED BY '\r\n';
