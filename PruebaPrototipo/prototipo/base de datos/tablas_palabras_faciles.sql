c8bad827001de849d7c621d54c09f14a5a7182ac
Irene Martin <imart02@ucm.es>
2019-05-12 15:04:24 +0200

    Añadida columna tag a las tablas de palabras faciles
    Co-Authored-By: pabloo17 <pabloo17@users.noreply.github.com>

--- PruebaPrototipo/prototipo/base de datos/tablas_palabras_faciles.sql
+++ PruebaPrototipo/prototipo/base de datos/tablas_palabras_faciles.sql
@@ -3,17 +3,20 @@ USE `mcr30`;
 
 CREATE TABLE IF NOT EXISTS `1000_palabras_faciles` (
   `word` varchar(100) COLLATE utf8_bin NOT NULL,
+  `tag`  varchar(100) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`word`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 
 CREATE TABLE IF NOT EXISTS `5000_palabras_faciles` (
   `word` varchar(100) COLLATE utf8_bin NOT NULL,
+  `tag`  varchar(100) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`word`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 
 CREATE TABLE IF NOT EXISTS `10000_palabras_faciles` (
   `word` varchar(100) COLLATE utf8_bin NOT NULL,
+  `tag`  varchar(100) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`word`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
